608602d274b498d557aeed1ba65773e6cc70aff8
Tim Pietrusky <timpietrusky@gmail.com>
2025-04-10 23:00:05 +0200

    chore: format file

--- handler.py
+++ handler.py
@@ -1,20 +1,22 @@
 import runpod
-import time  
+import time
+
 
 def handler(event):
     print(f"Worker Start")
-    input = event['input']
-    
-    prompt = input.get('prompt')  
-    seconds = input.get('seconds', 0)  
+    input = event["input"]
+
+    prompt = input.get("prompt")
+    seconds = input.get("seconds", 0)
 
     print(f"Received prompt: {prompt}")
     print(f"Sleeping for {seconds} seconds...")
-    
-    # Replace the sleep code with your Python function to generate images, text, or run any machine learning workload
-    time.sleep(seconds)  
-    
-    return prompt 
 
-if __name__ == '__main__':
-    runpod.serverless.start({'handler': handler })
+    # Replace the sleep code with your Python function to generate images, text, or run any machine learning workload
+    time.sleep(seconds)
+
+    return prompt
+
+
+if __name__ == "__main__":
+    runpod.serverless.start({"handler": handler})
